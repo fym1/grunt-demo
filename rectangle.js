6560568f0c841eecdba870f73bcbd812dd9183aa
fym1 <1668576168@qq.com>
2020-02-26 10:49:40 +0800

    Grunt ESLint 静态代码检查自动化任务

--- rectangle.js
+++ rectangle.js
@@ -1,25 +1,18 @@
-$(function(){
-    //get dom elem
-    var $width = $('#width'),
-        $height = $('#height'),
-        $bthCal = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area')
-    //*calc button click event
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-    }
-    $bthCal.click(function(){
-        //get value
-        var w = Number($width.val()),
-            h = Number($height.val());
-        //calculate
-        var p = 2*(w+h),
-            a = w*h
-        //output
-        $perimeter.val(p);
-        $area.val(a);
-        $perimeter.val(roundFractional(p, 1));
-        $area.val(roundFractional(a, 2));
-    })   
+$(function() {
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  
+  $btnCal.click(function(){
+    var w = Number($width.val()),
+        h = Number($height.val());
+  
+    var p = 2 * (w + h),
+        a = w * h;
+  
+    $perimeter.val(p);
+    $area.val(a);
+  });
 });
