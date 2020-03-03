9b822e2d027de7ca8892909d610569a859326230
fym1 <1668576168@qq.com>
2020-03-03 15:00:23 +0800

    html代码压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,19 @@
+    
 module.exports = function (grunt) {
   grunt.initConfig({
-    run:{
-      api:{
-        options:{wait:false},
-        args:['./app.js']
-      }
-    },
-    mochacli: {
-      all:['test/*.js'],
+    htmlmin: {
       options: {
-        reporter: 'spec',
-        bail:true
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-run');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('default', ['run','mochacli','stop:api']);
+  grunt.registerTask('default', ['htmlmin']); 
 };
